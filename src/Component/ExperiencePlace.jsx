4dff782e24b5f380b910f65e9601a418b148188b
Fikys1Bazilikys <fikysik69@gmail.com>
2025-07-01 18:20:04 +0300

    Add information about "Experience"

--- src/Component/ExperiencePlace.jsx
+++ src/Component/ExperiencePlace.jsx
@@ -1,12 +1,14 @@
 export default function ExperiencePlace (props) {
     return (
         <div className="experiencePlaceBox">
-            <div>
-                <h1 className="experiencePosition">{props.position}</h1>
-                <h3 className="third-heading">{props.company}</h3>
-                <p className="description-text">{props.description}</p>
+            <div style={{display: 'flex', marginBottom: '5rem', }}>
+                <div>
+                    <h1 className="experiencePosition">{props.position}</h1>
+                    <h3 className="third-heading">{props.company}</h3>
+                    <p className="description-text">{props.description}</p>
+                </div>
+                <data className="date-text">{props.date}</data>
             </div>
-            <data className="date-text">{props.date}</data>
         </div>
     )
 }
--- src/Data/ExperienceData.js
+++ src/Data/ExperienceData.js
@@ -2,23 +2,27 @@
 export default [
     {
         id: 1,
+        position: "Frontend Developer (Self-Taught)",
+        category: "Personal Projects",
+        description: " Designed and developed a personal portfolio website using React, Vite, and modular CSS. Focused on responsive design,\n" +
+            "      intuitive layout, and clean component structure. Currently working on a mobile application using React Native,\n" +
+            "      with emphasis on practical UX and real-world use cases.",
+        date: "2025 - Present",
+    },
+    {
+        id: 2,
         position: "QA Engineer",
         company: "TestMatick",
         description: "Worked as a QA tester on web and mobile application projects — performed manual testing, wrote test cases, and identified critical bugs in early development stages. Collaborated with developers and business analysts to ensure product quality, stability, and alignment with user requirements.",
         date: "2024 - Present",
     },
     {
-        id: 2,
-        position: "Experience",
-        company: "Full Stack Developer",
-        description: "Full Stack Developer",
-        date: "new",
-    },
-    {
         id: 3,
-        position: "Experience",
-        company: "Full Stack Developer",
-        description: "Full Stack Developer",
-        date: "new",
+        position: "Unity game developer",
+        company: "Solo Projects",
+        description: "Developed several 2D platformer prototypes in Unity, focusing on core gameplay mechanics such as player movement, physics interactions, enemy behavior, and level design. \n" +
+            "      Built reusable components for input handling and collision detection. \n" +
+            "      Gained strong experience in C# scripting and Unity’s component-based architecture.",
+        date: "2022 - 2024",
     },
 ]
