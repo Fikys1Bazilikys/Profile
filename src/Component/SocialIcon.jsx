ca161e55b5d551dfa5098c8a3d18030c3cd160a5
Fikys1Bazilikys <fikysik69@gmail.com>
2025-07-06 05:07:34 +0300

    style: adapt CSS styles for mobile screens (768px)

--- src/Component/SocialIcon.jsx
+++ src/Component/SocialIcon.jsx
@@ -5,7 +5,7 @@ export default function SocialIcon () {
         <div style={{ display: 'flex', gap: 26 }}>
             {socialData.map(({ id, icon: Icon, link, alt }) => (
                 <a href={link} key={id} target="_blank" rel="noopener noreferrer" title={alt}>
-                    <Icon size={60} color="#495057" />
+                    <Icon className="social-icon" />
                 </a>
             ))}
         </div>
