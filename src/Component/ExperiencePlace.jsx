a817b44bd5137a8dccbe8145f0cd7318bc374d31
Fikys1Bazilikys <fikysik69@gmail.com>
2025-07-06 03:31:08 +0300

    refactor: made a new Experience structure

--- src/Component/ExperiencePlace.jsx
+++ src/Component/ExperiencePlace.jsx
@@ -1,12 +1,21 @@
-export default function ExperiencePlace (props) {
+import experienceElements from '../Data/ExperienceData.js'
+
+export default function ExperiencePlace () {
+
+
+
     return (
         <div className="experiencePlaceBox">
-                <div>
-                    <h1 className="experiencePosition">{props.position}</h1>
-                    <h3 className="third-heading">{props.company}</h3>
-                    <p className="description-text">{props.description}</p>
+            {experienceElements.map(({ id, position,company,description,date}) => (
+                <div key={id}>
+                    <div>
+                        <h1 className="experiencePosition">{position}</h1>
+                        <h3 className="third-heading">{company}</h3>
+                        <p className="description-text">{description}</p>
+                    </div>
+                    <data className="date-text">{date}</data>
                 </div>
-                <data className="date-text">{props.date}</data>
+            ))}
         </div>
     )
 }
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,22 +1,12 @@
-import experienceData from "./Data/ExperienceData.js";
 import ExperiencePlace from "./Component/ExperiencePlace.jsx";
 
 export default function Experience () {
-
-    const ExperienceElements = experienceData.map ((element) => {
-        return (
-            <ExperiencePlace
-                key={element.id}
-                {...element}
-            />
-        )
-    })
 
     return (
         <section className="section-box" id="experience">
             <div className="section-content-box">
                 <h1 className="section-header">Experience</h1>
-                {ExperienceElements}
+                <ExperiencePlace />
             </div>
         </section>
     )
