36e1a316b4492b87f7bb2d6ddaea4d1f11cb0bf4
Fikys1Bazilikys <fikysik69@gmail.com>
2025-07-10 02:25:10 +0300

    style: adapt Certifications CSS styles for mobile screens

--- src/Certifications.jsx
+++ src/Certifications.jsx
@@ -1,13 +1,30 @@
+import { FaAward } from "react-icons/fa6";
+
+
+
 export default function Certifications () {
     return (
         <section className="section-box" id="certifications">
             <div className="section-content-box">
                 <h1 className="section-header">Certifications</h1>
                 <ul className="certificationsList" style={{ display: 'flex', flexDirection: 'column', gap: 16 }}>
-                    <li className="description-text">Cherkasy State Technological University - 2017 - 2021</li>
-                    <li className="description-text">Beginner course C# - School of education: "FoxmindEd" 2022</li>
-                    <li className="description-text">Automated Testing in .NET - EPAM CAMPUS 2024</li>
-                    <li className="description-text">English course -  GreenForest  2025 - Present</li>
+                    <li className="certification-item">
+                        <span><FaAward className="certification-cup" /></span>
+                        <p className="description-text">Cherkasy State Technological University - 2017 - 2021</p>
+                    </li>
+
+                    <li className="certification-item">
+                        <span><FaAward className="certification-cup" /></span>
+                        <p className="description-text">Beginner course C# - School of education: "FoxmindEd" 2022</p>
+                    </li>
+                    <li className="certification-item">
+                        <span><FaAward className="certification-cup" /></span>
+                        <p className="description-text">Automated Testing in .NET - EPAM CAMPUS 2024</p>
+                    </li>
+                    <li className="certification-item">
+                        <span><FaAward className="certification-cup" /></span>
+                        <p className="description-text">English course -  GreenForest  2025 - Present</p>
+                    </li>
                 </ul>
             </div>
         </section>
